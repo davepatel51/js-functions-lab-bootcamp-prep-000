81bcf5d38db8cbcada469ddc60dce72f1b80df43
Dave Patel <ddp2134@gmail.com>
2020-02-22 18:46:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function happyHolidays(){
   return("Happy holidays!");
 }
 
-
-function happyHolidaysto(name){
-  return('Happy holidays, ${name}');
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
 }
